1d7d88b520550c54ad1ed6d0c80a347c101594fd
skylerdj <jeffreyjoumjian@hotmail.com>
2020-12-22 06:10:33 +0200

    refactored code

--- generateText.js
+++ generateText.js
@@ -1,15 +1,46 @@
 const fs = require('fs');
+const say = require('say');
 const pdfParser = require('pdf-parse');
 
-let pdf = fs.readFileSync("./your_pdf.pdf"); // this is the path of the PDF you want to use
-readBook(pdf);
+// recommended size = 50000 || max size = 200000
+// decrease this if your OS throws and error.
+const MAX_SIZE = 200000;
 
 // read pdf and write text to output file
-function readBook(pdf) {
-	pdfParser(pdf).then((book) => {
+async function readBook(pdf) {
 
-		console.log(book.numpages);
-		fs.writeFileSync('./output.txt', book.text, 'utf-8');
+	const parsed = await pdfParser(pdf);
+	console.log(parsed.numpages);
+	fs.writeFileSync('output.txt', parsed.text, 'utf-8');
 
-	}).catch((err) => console.log(err));
+	return parsed;
 }
+
+function exportAudio() {
+	let text = fs.readFileSync('output.txt');
+	fs.mkdirSync('./audio', { recursive: true }, (err) => console.log(err));
+
+	let i = 0;
+	let numFiles = Math.floor(text.length / MAX_SIZE);
+	let remaining = Math.floor(text.length % MAX_SIZE);
+
+	for (i = 0; i < numFiles; i++) {
+		let curri = i;
+
+		let part = text.toString().substring(i * MAX_SIZE, (i + 1) * MAX_SIZE);
+
+		say.export(part, 'Samantha', 1, `./audio/part-${curri}.wav`, (err) => {
+			err ? console.error(err) : console.log(`Your text has been saved to part-${curri}.wav -> ${part.length / 1000}`);
+		});
+	}
+
+	// export remaining text to audio
+	part = text.toString().substring(i * MAX_SIZE, i * MAX_SIZE + remaining);
+	say.export(part, 'Samantha', 1, `./audio/part-${i}.wav`, (err) => {
+		err ? console.error(err) : console.log(`Your text has been saved to part-${i}.wav`);
+	});
+}
+
+
+
+module.exports = { readBook, exportAudio }
--- index.js
+++ index.js
@@ -1,41 +1,14 @@
 const fs = require('fs');
-const say = require('say');
-
-
-// recommended size = 50000 || max size = 200000
-// decrease this if your OS throws and error.
-const MAX_SIZE = 50000;
+const { readBook, exportAudio } = require('./generateText');
 
 
 
-// Make sure to manually remove all the additional text before and after the chapters such as toc, indices... before calling this
-// Note this might take some time as the create files may be really large
-// Optionally decrease the MAX_SIZE to have smaller files
-exportAudio();
+let pdf = fs.readFileSync("/Users/mbp/Desktop/LAU/courses/Fall-2020/SOC-480/Project/Universities Shift to Online Learning.pdf"); // this is the path of the PDF you want to use
+getAudioFromText();
 
 
-function exportAudio() {
-	let text = fs.readFileSync('./output.txt');
-	fs.mkdirSync('./audio', { recursive: true }, (err) => console.log(err));
-
-	let i = 0;
-	let numFiles = Math.floor(text.length / MAX_SIZE);
-	let remaining = Math.floor(text.length % MAX_SIZE);
-
-	for (i = 0; i < numFiles; i++) {
-		let curri = i;
-
-		let part = text.toString().substring(i * MAX_SIZE, (i + 1) * MAX_SIZE);
-
-		say.export(part, 'Samantha', 1, `./audio/part-${curri}.wav`, (err) => {
-			err ? console.error(err) : console.log(`Your text has been saved to part-${curri}.wav`);
-		});
-	}
-
-	// export remaining text to audio
-	part = text.toString().substring(i * MAX_SIZE, i * MAX_SIZE + remaining);
-	say.export(part, 'Samantha', 1, `./audio/part-${i}.wav`, (err) => {
-		err ? console.error(err) : console.log(`Your text has been saved to part-${i}.wav`);
-	});
+async function getAudioFromText() {
+	let book = await readBook(pdf);
+	if (book)
+		exportAudio();
 }
-
